8e139aa94d2f8127e3034f7bb283c11d2c2039e0
egorkuzn <egorkuzn2002@mail.ru>
2022-09-03 01:10:24 +0700

    generator of rand strings done and random strings

---
+++ lab-6/strings_generator.c
@@ -0,0 +1,40 @@
+//Yegor Kuznetsov, 2022, NSU
+//This program generates 100 random strings and write in file.
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFAULT_COUNT 100
+#define DEFAULT_FILENAME "string_collection.txt"
+
+int randomSize(int* size){
+    *size = rand() % DEFAULT_COUNT;
+    return *size;
+}
+
+char randChar(){
+    return '/' + rand() % (16 * 4);
+}
+
+void initBuffer(char* buffer, int size){
+    for(int i = 0; i < size; i++)
+        buffer[i] = randChar();
+}
+
+int main(){
+    FILE* out = fopen(DEFAULT_FILENAME, "w");
+
+    if(!out){
+        printf("Didn't opened");
+        return 1;
+    }
+
+    for(int i = 0; i < DEFAULT_COUNT; i++){
+        int size = 0;
+        char* buffer = calloc(randomSize(&size), sizeof(char));
+        initBuffer(buffer, size);
+        fprintf(out, "%s\n", buffer);
+    }
+
+    fclose(out);
+    return 0;
+}
