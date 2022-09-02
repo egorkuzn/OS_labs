0ceb4f4b3319138dc05fa3f6bd9b11cc134d78a8
egorkuzn <egorkuzn2002@mail.ru>
2022-09-03 00:22:01 +0700

    5 done: I need to be more attentive when use files

---
+++ lab-5/5.c
@@ -0,0 +1,55 @@
+//Yegor Kuznetsov, 2022, NSU
+//Child prints text while parent don't stop.
+//Child should notify when finish running.
+//Parent stop child after 2 seconds.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+#define DEFAULT_FILE_NAME "index.html"
+
+void cleanupNotification(void* param){
+    printf("\n---\n");
+    printf("FINISH\n");
+}
+
+void* printer(void* param){
+    pthread_cleanup_push(cleanupNotification, NULL);
+    FILE* in;
+    in = fopen(DEFAULT_FILE_NAME, "r");
+    char buff[BUFSIZ];    
+
+    while(fscanf(in, "%s", buff))       
+        printf("%s ", buff);
+
+    fclose(in);
+    pthread_cleanup_pop(1);    
+    return NULL;
+}
+
+void pthreadFailure(int code, char problem[], char* argv[]){
+    if(code){
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: %s thread: %s\n", argv[0], problem, buf);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char *argv[]){   
+    pthread_t thread;     
+    int code;
+
+    code = pthread_create(&thread, NULL, printer, NULL);
+    pthreadFailure(code, "creating", argv);
+
+    sleep(2);
+
+    code = pthread_cancel(thread);
+    pthreadFailure(code, "canceling", argv);
+    pthread_exit(NULL);
+    exit(EXIT_SUCCESS);
+}
