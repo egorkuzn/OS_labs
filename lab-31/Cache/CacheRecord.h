1b9ca96e5c53daf0c07941a7e3d32814eb9aa517
egorkuzn <egorkuzn2002@mail.ru>
2022-12-05 13:37:12 +0700

    CacheRecord class added

---
+++ lab-31/Cache/CacheRecord.h
@@ -0,0 +1,16 @@
+//
+// Created by egork on 05.12.2022.
+//
+
+#ifndef OS_LABS_CACHERECORD_H
+#define OS_LABS_CACHERECORD_H
+
+namespace lab31 {
+
+    class CacheRecord {
+
+    };
+
+} // lab31
+
+#endif //OS_LABS_CACHERECORD_H
