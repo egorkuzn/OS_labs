22ea56172314a48b04517f27efa077b02dbfc7b8
egorkuzn <egorkuzn2002@mail.ru>
2022-12-05 13:33:31 +0700

    proxy class added

---
+++ lab-31/Proxy.cpp
@@ -0,0 +1,8 @@
+//
+// Created by egork on 05.12.2022.
+//
+
+#include "Proxy.h"
+
+namespace lab31 {
+} // lab31
---
+++ lab-31/Proxy.h
@@ -0,0 +1,16 @@
+//
+// Created by egork on 05.12.2022.
+//
+
+#ifndef OS_LABS_PROXY_H
+#define OS_LABS_PROXY_H
+
+namespace lab31 {
+
+    class Proxy {
+
+    };
+
+} // lab31
+
+#endif //OS_LABS_PROXY_H
