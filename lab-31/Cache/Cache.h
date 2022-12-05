59dc0fc2d0cc61372dc033eb02ed6aa004e4b7d0
egorkuzn <egorkuzn2002@mail.ru>
2022-12-05 13:36:23 +0700

    Cache class added

---
+++ lab-31/Cache/Cache.h
@@ -0,0 +1,16 @@
+//
+// Created by egork on 05.12.2022.
+//
+
+#ifndef OS_LABS_CACHE_H
+#define OS_LABS_CACHE_H
+
+namespace lab31 {
+
+    class Cache {
+
+    };
+
+} // lab31
+
+#endif //OS_LABS_CACHE_H
