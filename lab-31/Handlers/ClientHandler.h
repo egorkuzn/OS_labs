bc4fff80089ccde1b9ed6efa7d67d8ce2efa4910
egorkuzn <egorkuzn2002@mail.ru>
2022-12-05 13:50:17 +0700

    Connection and Client handlers headers done

--- lab-31/Handlers/ClientHandler.h
+++ lab-31/Handlers/ClientHandler.h
@@ -7,7 +7,75 @@
 
 namespace lab31 {
 
-    class ClientHandler {
+    class Proxy;
+    class http_parser;
+
+    class ClientHandler: public ConnectionHandler{
+    public:
+        ~ClientHandler() override{proxy->getCache()->unsubscribe(url, clientSocket);};
+
+        explicit ClientHandler(int socket, Proxy *proxy);
+
+        int connectToServer(const std::string& host);
+
+        bool handle(int event) override;
+
+        void setURL(const char *at, size_t len);
+
+        void setHost(const char *at, size_t len);
+
+        int getSocket() const { return clientSocket; }
+
+        std::string getLastField();
+
+        void setLastField(const char *at, size_t len);
+
+        void resetLastField();
+
+        void createServer(int socket);
+
+        bool writeToServer(const std::string& msg);
+
+        bool tryMakeFirstWriter();
+
+        bool RequestParser();
+
+        bool initConnectionToDest();
+
+        static std::string getUrl(std::string in);
+
+        static std::string getMethod(std::string in);
+
+        static std::string getPrVersion(std::string in);
+
+        void deleteCache() override;
+
+        size_t getReadElements() override;
+
+        CacheRecord* getCacheRecord() override { return  record;}
+
+    private:
+        int timeAlive = 100;
+        int clientSocket;
+        Proxy *proxy;
+        std::string request;
+        std::string lastField;
+        std::string prVersion = "1.0";
+        std::string url;
+        std::string host;
+        std::string port;
+        ServerHandler *server;
+        CacheRecord *record;
+        size_t readPointer = 0;
+        bool cachingInParallel = false;
+        bool firstWriter = false;
+
+        bool receive();
+
+        bool becomeFirstWriter();
+
+        bool initialized = false;
+
 
     };
 
--- lab-31/Handlers/ConnectionHandler.h
+++ lab-31/Handlers/ConnectionHandler.h
@@ -5,4 +5,20 @@
 #ifndef OS_LABS_CONNECTIONHANDLER_H
 #define OS_LABS_CONNECTIONHANDLER_H
 
+namespace lab31 {
+    class ConnectionHandler {
+    public:
+        virtual ~ConnectionHandler() = default;
+
+        virtual void deleteCache() = 0;
+
+        virtual CacheRecord* getCacheRecord() = 0;
+
+        virtual bool handle(int event) = 0;
+
+        virtual size_t getReadElements() = 0;
+    private:
+    };
+}
+
 #endif //OS_LABS_CONNECTIONHANDLER_H
