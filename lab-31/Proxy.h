7a5784f552c511b974099046d2b23e44d6bf3a0e
egorkuzn <egorkuzn2002@mail.ru>
2022-12-05 14:06:47 +0700

    Proxy.h done

--- lab-31/Proxy.h
+++ lab-31/Proxy.h
@@ -8,7 +8,44 @@
 namespace lab31 {
 
     class Proxy {
+    private:
+        sockaddr_in address{};
+        int socketDesc;     // сокет сервера
+        std::vector<pollfd> connections{};
+        std::map<int, ConnectionHandler *> handlers;
+        bool proxyOn = false;
+        Cache *cache;
 
+        void disconnectHandler(int _sock);
+
+    public:
+        Cache *getCache() { return cache; };
+
+        void addNewHandler(int fd, ConnectionHandler *handler);
+
+        explicit Proxy(int port);
+
+        ConnectionHandler *getHandlerByFd(int fd);
+
+        ~Proxy();
+
+        int createProxySocket(int port);
+
+        bool initProxySocket();
+
+        void addNewConnection(int socket, short event);
+
+        void run();
+
+        void stopProxy();
+
+        void addEvent(int fd, short event);
+
+        void deleteEvent(int fd, short event);
+
+        void makeNewServer(const std::vector<int> &observers);
+
+        void setPollOutEventToObservers(const std::vector<int> &observers);
     };
 
 } // lab31
